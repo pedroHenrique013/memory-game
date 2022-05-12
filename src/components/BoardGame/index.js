99e3b619d68b6cb652906e0de6bdf8d96d679d83
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-12 10:58:08 -0300

    :art: Add um comentario html

--- src/components/BoardGame/index.js
+++ src/components/BoardGame/index.js
@@ -7,7 +7,7 @@ export function BoardGame (amountCard) {
     const $htmlCardGame = CardGame();
     const $htmlContent = $htmlCardGame.repeat(amountCard);
 
-    return `
+    return /*html*/`
       <section class="board-game">
         ${$htmlContent}
       </section>
--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,7 +1,7 @@
 import './style.css'
 
 export function CardGame() {
-    return `
+    return /*html*/`
         <article class="card-game">
             <img src="images/logo-card.png" alt="Imgem de um ponto de interrogação branco" />
         </article>

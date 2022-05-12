a905ca264419d01fb75e6181935775f17fc2f965
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-12 14:18:27 -0300

    :sparkles: Add funcionalidade de flip do CardGame

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,11 +1,16 @@
+import './style.css';
 import { CardGame } from './../CardGame/index';
 
 
 export function CardFrontBack() {
     return /*html*/`
         <article class="card-front-back">
-            ${CardGame()}
-            ${CardGame('logo-javascript', 'Logo javascript')}
+            <div class="card -front">
+                ${CardGame()}
+            </div>
+            <div class="card -back">
+                ${CardGame('logo-javascript', 'Logo javascript')}
+            </div>
         </article>
     `;
 }

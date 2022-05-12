e46a44416f47ab7eac30734df53f1e183b986f3a
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-12 10:55:20 -0300

    :sparkles: Add ScoreBoard

--- main.js
+++ main.js
@@ -2,16 +2,14 @@ import './src/styles/reset.css';
 import './src/styles/global.css';
 
 import { BoardGame } from './src/components/BoardGame/index';
-import { PlayerName } from './src/components/PlayerName';
+import { ScoreBoard } from './src/components/ScoreBoard';
 
 
 const $root = document.querySelector('#root');
 
 $root.insertAdjacentHTML(
   'beforeend',
-    `
-      ${PlayerName("Player1")}
-      ${PlayerName("Player2")}
+    ` ${ScoreBoard()}
       ${BoardGame(6)}
     `
 );

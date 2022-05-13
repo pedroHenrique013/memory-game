494615b17aa38b2df0dc88ee8133f05a6d349504
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-13 13:27:35 -0300

    :lipstick: Add css no componente PlayerView

---
+++ src/components/PlayerView/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export function PlayerView(currentPlayer = 1) {
+  return /*html*/ `
+    <img class="player-view"
+         data-currentPlayer="${currentPlayer}"
+         src="images/IconArrowDown.png" 
+         alt="Icone de uma seta para baixo" 
+    />
+  `;
+}
--- src/components/ScoreBoard/index.js
+++ src/components/ScoreBoard/index.js
@@ -1,12 +1,14 @@
 import './style.css';
 import { PlayerName } from './../PlayerName/index';
 import { PlayerScore } from './../PlayerScore/index';
+import { PlayerView } from '../PlayerView';
 
 
 
 export function ScoreBoard() {
   return /*html*/`
     <header class="score-board">
+      ${PlayerView(1)}
       ${PlayerName("Player1")}
       ${PlayerScore()}
       <p class="versus">vs</p>

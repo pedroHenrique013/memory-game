d072d1dfd7c1929387392d2b00ee543c3171c575
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-13 10:34:40 -0300

    :sparkles: Criado componente PointsGame

---
+++ src/components/PointsGame/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+export function PointsGame() {
+  return /*html*/`
+    <div class="points-game">
+      <div class="points"></div>
+      <div class="points"></div>
+      <div class="points"></div>
+    </div>
+  `;
+}

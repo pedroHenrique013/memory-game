e8b5b8cb2384731f6685478a0055c3e040d3948e
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-13 10:46:17 -0300

    :sparkles: Add o componente PointsGame

--- src/components/ScoreBoard/index.js
+++ src/components/ScoreBoard/index.js
@@ -1,11 +1,15 @@
 import './style.css';
 import { PlayerName } from './../PlayerName/index';
+import { PointsGame } from './../PointsGame/index';
 
 
 export function ScoreBoard() {
   return /*html*/`
     <header class="score-board">
       ${PlayerName("Player1")}
+      ${PointsGame()}
+      <p class="versus">vs</p>
+      ${PointsGame()}
       ${PlayerName("Player2")}
     </header>
   `;

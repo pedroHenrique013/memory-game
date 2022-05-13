cce0155f6d8213fcd794b369c2205cf60996833e
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-13 14:31:12 -0300

    :sparkles: Mudança de localização da pagina para pt-br

---
+++ src/components/BoardGame/dados.js
@@ -0,0 +1,26 @@
+export const cards = [
+    {
+      icon: 'logo-html' , 
+      alt: 'Logo do HTML'
+    },
+    {
+      icon: 'logo-html' , 
+      alt: 'Logo do HTML'
+    },
+    {
+      icon: 'logo-css' ,
+      alt: 'Logo do CSS'
+    },
+    {
+      icon: 'logo-css' ,
+      alt: 'Logo do CSS'
+    },
+    {
+      icon: 'logo-javascript' ,
+      alt: 'Logo do JavaScript'
+    },
+    {
+      icon: 'logo-javascript' ,
+      alt: 'Logo do JavaScript'
+    }
+  ]
--- src/components/BoardGame/index.js
+++ src/components/BoardGame/index.js
@@ -1,15 +1,18 @@
 import './style.css';
 
 import { CardFrontBack } from '../CardFrontBack';
+import { cards } from './dados';
 
 
 export function BoardGame (amountCard) {
-    const $htmlCardFrontBack = CardFrontBack();
-    const $htmlContent = $htmlCardFrontBack.repeat(amountCard);
+  
+    const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.alt));
+    const $htmlCards = htmlCardsList.join('');
+    console.log($htmlCards);
 
     return /*html*/`
       <section class="board-game">
-        ${$htmlContent}
+        ${$htmlCards}
       </section>
     `;
 }

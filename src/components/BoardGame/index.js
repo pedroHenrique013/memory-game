899efb8c28019a9577a306dec3ba653ab4505d51
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-13 15:47:48 -0300

    :bug: Swap player when flip two cards

--- src/components/BoardGame/index.js
+++ src/components/BoardGame/index.js
@@ -5,14 +5,35 @@ import { cards } from './dados';
 
 
 export function BoardGame (amountCard) {
-  
-    const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.alt));
-    const $htmlCards = htmlCardsList.join('');
-    console.log($htmlCards);
+  const flipHideCards = ($cardsActive) => {
+    $cardsActive.forEach((card) => card.classList.remove("-active"));
+  }
 
-    return /*html*/`
-      <section class="board-game">
+  const swapPlayer = () => {
+    const $playerView = document.querySelector('.player-view');
+    const currentPlayer = $playerView.getAttribute('data-currentPlayer');
+    $playerView.setAttribute("data-currentPlayer", currentPlayer == '1' ? '2' : '1');
+  }
+
+  window.boardGame = {}
+  window.boardGame.handleClick = () => {
+    const $boardGame = document.querySelector('.board-game');
+    const $cardsActive = $boardGame.querySelectorAll(".card-front-back.-active");
+
+    if ($cardsActive.length === 2) {
+      setTimeout(() => {
+        
+        flipHideCards($cardsActive);
+        swapPlayer();
+      }, 1000);
+    }
+  }
+  const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.alt));
+  const $htmlCards = htmlCardsList.join('');
+
+  return /*html*/`
+    <section class="board-game" onClick="boardGame.handleClick()">
         ${$htmlCards}
-      </section>
-    `;
+    </section>
+  `;
 }

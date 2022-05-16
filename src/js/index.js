e7305a9f4d73f36662ec3851603f8785f5549f4f
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-16 18:31:30 -0300

    :construction: Modificado a estrutura de condição if else

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,22 @@
 export function cardsCompare() {
   const $cardsActive = document.querySelectorAll(".-active");
   const $cardsImgs = document.querySelectorAll(".-active .-back #img");
-  const points = document.getElementById('points');
-  
+  const points = document.getElementById("points");
 
   if ($cardsActive.length === 2) {
     console.log($cardsImgs);
-    $cardsImgs[0].getAttribute("src") === $cardsImgs[1].getAttribute("src")
-    console.log("Acertou");
-    points.dataset.points = parseInt(points.dataset.points) + 1;
-    toString(points);
-      
+
+    if ($cardsImgs[0].getAttribute("src") === $cardsImgs[1].getAttribute("src")) {
+      console.log("Acertou");
+      points.dataset.points = parseInt(points.dataset.points) + 1;
+      toString(points);
     } else {
       setTimeout(() => {
         flipHideCards($cardsActive);
         swapPlayer();
       }, 1000);
     }
+  }
 }
 
 const flipHideCards = ($cardsActive) => {

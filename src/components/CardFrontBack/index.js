d2b14e852d853cdbea8efdda15556219523fd7e4
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-19 10:22:31 -0300

    :sparkles: add funcionalidade de comparação dos cards
    Adicionado uma das regras do jogo, como a comparação se as cartas viradas são iguais ou diferentes e controle dos pontos dos jogadores.

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,22 +1,74 @@
-import './style.css';
-import { CardGame } from './../CardGame/index';
-
+import "./style.css";
+import { CardGame } from "./../CardGame/index";
+import { flipHideCards } from "../../js";
+import { swapPlayer } from "./../../js/index";
 
 export function CardFrontBack(icon, alt) {
-    window.cardFrontBack = {}
-    window.cardFrontBack.handleClick = (event) => {
-        const $origin = event.target;
-        const $cardFrontBack = $origin.closest('.card-front-back');
+  let hasFlippedCard = false;
+  let firstCard, secondCard;
 
-        $cardFrontBack.classList.toggle('-active');
+  window.cardFrontBack = {};
+  window.cardFrontBack.handleClick = (event) => {
+    const $origin = event.target;
+    const $cardFrontBack = $origin.closest(".card-front-back");
+    const $firstCard = $cardFrontBack
+      .querySelector(".card.-back #img")
+      .getAttribute("src");
+
+    $cardFrontBack.classList.add("-active");
+
+    /* --------------- Logica das Cartas ------------------------*/
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = $firstCard;
+      console.log(firstCard);
+      return firstCard;
     }
-    
-    return /*html*/`
+
+    hasFlippedCard = false;
+    secondCard = $firstCard;
+    const $cardsActive = document.querySelectorAll(".-active");
+    const $score = document.getElementsByTagName("ol");
+    const $playerScore = document.querySelector(".player-score");
+    const $currentScore = $playerScore.getAttribute("data-currentScore");
+    const $card = document.querySelector(".card-front-back");
+
+    if (firstCard === secondCard) {
+      if ($playerScore.getAttribute("data-currentScore") === "1") {
+        $score[0].dataset.points = parseInt($score[0].dataset.points) + 1;
+        toString($score[0].dataset.points);
+      }
+
+      if ($playerScore.getAttribute("data-currentScore") === "2") {
+        $score[1].dataset.points = parseInt($score[1].dataset.points) + 1;
+        toString($score[1].dataset.points);
+      }
+      $cardsActive.forEach((card) => {
+        card.classList.add("-flip")
+        card.setAttribute("onclick", "return false");
+        
+      });
+      
+    } else {
+      setTimeout(() => {
+        flipHideCards($cardsActive);
+        swapPlayer();
+
+        $playerScore.setAttribute(
+          "data-currentScore",
+          $currentScore == "1" ? "2" : "1"
+        );
+      }, 1000);
+    }
+  };
+
+  return /*html*/ `
         <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()}
             </div>
-            <div class="card -back">
+            <div class="card -back" >
                 ${CardGame(icon, alt)}
             </div>
         </article>

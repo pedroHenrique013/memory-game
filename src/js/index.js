a8f1420284952dc73d40265785720c8311165451
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-19 10:19:39 -0300

    :sparkles: Add funcionalidades de embaralhar cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,43 @@
-export function cardsCompare() {
-  const $cardsActive = document.querySelectorAll(".-active");
-  const $cardsImgs = document.querySelectorAll(".-active .-back #img");
-  const points = document.getElementById("points");
+import { cards } from './dados';
+import { CardFrontBack } from './../components/CardFrontBack/index';
+import { PlayerName } from './../components/PlayerName/index';
 
-  if ($cardsActive.length === 2) {
-    console.log($cardsImgs);
-
-    if ($cardsImgs[0].getAttribute("src") === $cardsImgs[1].getAttribute("src")) {
-      console.log("Acertou");
-      points.dataset.points = parseInt(points.dataset.points) + 1;
-      toString(points);
-    } else {
-      setTimeout(() => {
-        flipHideCards($cardsActive);
-        swapPlayer();
-      }, 1000);
-    }
-  }
-}
-
-const flipHideCards = ($cardsActive) => {
+export const flipHideCards = ($cardsActive) => {
   $cardsActive.forEach((card) => card.classList.remove("-active"));
 };
 
-const swapPlayer = () => {
+export const swapPlayer = () => {
   const $playerView = document.querySelector(".player-view");
   const currentPlayer = $playerView.getAttribute("data-currentPlayer");
+
   $playerView.setAttribute(
     "data-currentPlayer",
     currentPlayer == "1" ? "2" : "1"
   );
 };
+
+export const shuffle = () => {
+
+  const shuffleCards = cards.sort(() => Math.floor(Math.random() * 6));
+  const $htmlCards = shuffleCards.map((card) => CardFrontBack(card.icon, card.alt, card.id));//
+  return $htmlCards;
+  
+}
+
+export const orderCards = () => {
+  cardSort.sort((a, b) => {
+    if (a.id > b.id) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
+cards.forEach((elemento) => {
+  if (!elemento.id) {
+    elemento.id = Math.floor(Math.random() * 24);
+  }
+});
+
+const cardSort = cards;

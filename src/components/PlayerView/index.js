fd411291fe20aa2d07b35c358b18f3185e7e945c
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-19 14:16:35 -0300

    :art: Add caminho das imagens

--- src/components/PlayerView/index.js
+++ src/components/PlayerView/index.js
@@ -4,7 +4,7 @@ export function PlayerView(currentPlayer = 1) {
   return /*html*/ `
     <img class="player-view"
          data-currentPlayer="${currentPlayer}"
-         src="../../../src/images/IconArrowDown.png" 
+         src="../../../src/assets/IconArrowDown.png" 
          alt="Icone de uma seta para baixo" 
     />
   `;

32b8bafac5116cee01868d97fd0183170c165efa
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-19 10:36:16 -0300

    :art: Formatação da estrutura

--- src/js/dados.js
+++ src/js/dados.js
@@ -1,26 +1,32 @@
 export const cards = [
-    {
-      icon: 'logo-html' , 
-      alt: 'Logo do HTML'
-    },
-    {
-      icon: 'logo-html' , 
-      alt: 'Logo do HTML'
-    },
-    {
-      icon: 'logo-css' ,
-      alt: 'Logo do CSS'
-    },
-    {
-      icon: 'logo-css' ,
-      alt: 'Logo do CSS'
-    },
-    {
-      icon: 'logo-javascript' ,
-      alt: 'Logo do JavaScript'
-    },
-    {
-      icon: 'logo-javascript' ,
-      alt: 'Logo do JavaScript'
-    }
-  ]
+  {
+    icon: "logo-html",
+    alt: "Logo do HTML",
+    
+  },
+  {
+    icon: "logo-html",
+    alt: "Logo do HTML",
+    
+  },
+  {
+    icon: "logo-css",
+    alt: "Logo do CSS",
+    
+  },
+  {
+    icon: "logo-css",
+    alt: "Logo do CSS",
+    
+  },
+  {
+    icon: "logo-javascript",
+    alt: "Logo do JavaScript",
+    
+  },
+  {
+    icon: "logo-javascript",
+    alt: "Logo do JavaScript",
+    
+  },
+];

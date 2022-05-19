ec3eed27f593265cb886fcb77aa08549e4b5a0d8
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-19 10:26:44 -0300

    :art: Add um mais um atributo na tag ol para controle dos pontos

--- src/components/PlayerScore/index.js
+++ src/components/PlayerScore/index.js
@@ -1,9 +1,9 @@
 import { PlayerView } from '../PlayerView';
 import './style.css';
 
-export function PlayerScore(points = 0) {
+export function PlayerScore(points, currentScore) {
   return /*html*/`
-    <ol id="points" data-points="${points}" class="player-score">
+    <ol id="points" data-points="${points}" data-currentScore="${currentScore}" class="player-score">
       <li class="points"></li>
       <li class="points"></li>
       <li class="points"></li>

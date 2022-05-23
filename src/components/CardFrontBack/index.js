7f667d2d0e488e1ff3d8b57e6e7aa5766f697b01
pedroHenrique013 <henrique.netsto@gmail.com>
2022-05-23 13:27:52 -0300

    :construction: Refatoração

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,66 +1,12 @@
 import "./style.css";
 import { CardGame } from "./../CardGame/index";
-import { flipHideCards } from "../../js";
-import { swapPlayer } from "./../../js/index";
+import { logicGame } from "../../js";
 
 export function CardFrontBack(icon, alt) {
-  let hasFlippedCard = false;
-  let firstCard, secondCard;
 
   window.cardFrontBack = {};
   window.cardFrontBack.handleClick = (event) => {
-    const $origin = event.target;
-    const $cardFrontBack = $origin.closest(".card-front-back");
-    const $firstCard = $cardFrontBack
-      .querySelector(".card.-back #img")
-      .getAttribute("src");
-
-    $cardFrontBack.classList.add("-active");
-
-    /* --------------- Logica das Cartas ------------------------*/
-
-    if (!hasFlippedCard) {
-      hasFlippedCard = true;
-      firstCard = $firstCard;
-      console.log(firstCard);
-      return firstCard;
-    }
-
-    hasFlippedCard = false;
-    secondCard = $firstCard;
-    const $cardsActive = document.querySelectorAll(".-active");
-    const $score = document.getElementsByTagName("ol");
-    const $playerScore = document.querySelector(".player-score");
-    const $currentScore = $playerScore.getAttribute("data-currentScore");
-    const $card = document.querySelector(".card-front-back");
-
-    if (firstCard === secondCard) {
-      if ($playerScore.getAttribute("data-currentScore") === "1") {
-        $score[0].dataset.points = parseInt($score[0].dataset.points) + 1;
-        toString($score[0].dataset.points);
-      }
-
-      if ($playerScore.getAttribute("data-currentScore") === "2") {
-        $score[1].dataset.points = parseInt($score[1].dataset.points) + 1;
-        toString($score[1].dataset.points);
-      }
-      $cardsActive.forEach((card) => {
-        card.classList.add("-flip")
-        card.setAttribute("onclick", "return false");
-        
-      });
-      
-    } else {
-      setTimeout(() => {
-        flipHideCards($cardsActive);
-        swapPlayer();
-
-        $playerScore.setAttribute(
-          "data-currentScore",
-          $currentScore == "1" ? "2" : "1"
-        );
-      }, 1000);
-    }
+      logicGame(event);
   };
 
   return /*html*/ `
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,79 @@
-import { cards } from './dados';
-import { CardFrontBack } from './../components/CardFrontBack/index';
-import { PlayerName } from './../components/PlayerName/index';
+import { cards } from "./dados";
+import { CardFrontBack } from "./../components/CardFrontBack/index";
+import { PlayerName } from "./../components/PlayerName/index";
 
-export const flipHideCards = ($cardsActive) => {
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+export const logicGame = (event) => {
+
+  /*-------------- Add class -active to card --------------*/
+  const $origin = event.target;
+  const $cardFrontBack = $origin.closest(".card-front-back");
+  const $firstCard = $cardFrontBack
+    .querySelector(".card.-back #img")
+    .getAttribute("src");
+  $cardFrontBack.classList.add("-active");
+  
+  if ($cardFrontBack.getAttribute("class") === "card-front-back -active") {
+    $cardFrontBack.setAttribute("onclick", "return false");
+  }
+
+
+
+  /*-------------- If hasFlippedCard is false, save firstCard --------------*/
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = $firstCard;
+    console.log(firstCard);
+    return firstCard;
+  }
+
+  hasFlippedCard = false;
+  secondCard = $firstCard;
+  const $cardsActive = document.querySelectorAll(".-active");
+  const $score = document.getElementsByTagName("ol");
+  const $playerScore = document.querySelector(".player-score");
+  const $currentScore = $playerScore.getAttribute("data-currentScore");
+
+  /*-------------- If firstCard is equal to secondCard, hide cards --------------*/
+  if (firstCard === secondCard) {
+    
+    if ($playerScore.getAttribute("data-currentScore") === "1") {
+      $score[0].dataset.points = parseInt($score[0].dataset.points) + 1;
+      toString($score[0].dataset.points);
+    }
+
+    if ($playerScore.getAttribute("data-currentScore") === "2") {
+      $score[1].dataset.points = parseInt($score[1].dataset.points) + 1;
+      toString($score[1].dataset.points);
+    }
+    $cardsActive.forEach((card) => {
+      card.classList.add("-flip");
+      card.setAttribute("onclick", "return false");
+    });
+
+  } else { 
+    setTimeout(() => {
+      flipHideCards($cardsActive);
+      swapPlayer();
+
+      $playerScore.setAttribute(
+        "data-currentScore",
+        $currentScore == "1" ? "2" : "1"
+      );
+      $cardsActive.forEach((card) => {
+        card.setAttribute("onclick", "cardFrontBack.handleClick(event)");
+      });
+    }, 1000);
+  }
+};
+
+const flipHideCards = ($cardsActive) => {
   $cardsActive.forEach((card) => card.classList.remove("-active"));
 };
 
-export const swapPlayer = () => {
+const swapPlayer = () => {
   const $playerView = document.querySelector(".player-view");
   const currentPlayer = $playerView.getAttribute("data-currentPlayer");
 
@@ -17,12 +84,12 @@ export const swapPlayer = () => {
 };
 
 export const shuffle = () => {
-
   const shuffleCards = cards.sort(() => Math.floor(Math.random() * 6));
-  const $htmlCards = shuffleCards.map((card) => CardFrontBack(card.icon, card.alt, card.id));//
+  const $htmlCards = shuffleCards.map((card) =>
+    CardFrontBack(card.icon, card.alt, card.id)
+  ); //
   return $htmlCards;
-  
-}
+};
 
 export const orderCards = () => {
   cardSort.sort((a, b) => {
@@ -32,7 +99,7 @@ export const orderCards = () => {
       return -1;
     }
   });
-}
+};
 
 cards.forEach((elemento) => {
   if (!elemento.id) {
@@ -41,3 +108,5 @@ cards.forEach((elemento) => {
 });
 
 const cardSort = cards;
+
+
